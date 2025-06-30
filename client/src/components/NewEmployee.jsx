e4bc7aedfe34030635c0e00d22199ac77809bb60
soheil <soheilkooshan710@gmail.com>
2025-07-01 01:34:30 +0330

    some style added

--- client/src/components/NewEmployee.jsx
+++ client/src/components/NewEmployee.jsx
@@ -4,6 +4,7 @@ import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogTitle from "@mui/material/DialogTitle";
 import { useState } from "react";
+import Box from "@mui/material/Box";
 
 function NewEmployee() {
   const [open, setOpen] = useState(false);
@@ -17,29 +18,40 @@ function NewEmployee() {
   };
 
   return (
-    <div>
+    <div style={{ padding: "1rem" }}>
       <Button variant="outlined" onClick={handleClickOpen}>
         New Employee
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <TextField
-          required
-          label="First Name"
-          id="firstName"
-          variant="outlined"
-        />
-        <TextField
-          required
-          label="Last Name"
-          id="lastName"
-          variant="outlined"
-        />
-        <TextField required label="Job" id="job" variant="outlined" />
-        <DialogActions>
-          <Button onClick={handleClose}>CANCEL</Button>
-          <Button type="submit">ADD</Button>
-        </DialogActions>
+        <DialogTitle>Add Employee</DialogTitle>
+        <form>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "1.5rem",
+              gap: "1rem",
+            }}
+          >
+            <TextField
+              required
+              label="First Name"
+              id="firstName"
+              variant="outlined"
+            />
+            <TextField
+              required
+              label="Last Name"
+              id="lastName"
+              variant="outlined"
+            />
+            <TextField required label="Job" id="job" variant="outlined" />
+            <DialogActions>
+              <Button onClick={handleClose}>CANCEL</Button>
+              <Button type="submit">ADD</Button>
+            </DialogActions>
+          </Box>
+        </form>
       </Dialog>
     </div>
   );
