261a73d877dec9831065358e71f69aeee91c0730
soheil <soheilkooshan710@gmail.com>
2025-07-05 16:23:44 +0330

    some styles added

--- client/src/components/RatingEmployee.jsx
+++ client/src/components/RatingEmployee.jsx
@@ -4,9 +4,18 @@ import Box from "@mui/material/Box";
 import { useState } from "react";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import Typography from "@mui/material/Typography";
+import BarChartIcon from "@mui/icons-material/BarChart";
 
 function RatingEmployee() {
   const [value, setValue] = useState(null);
+
+  const style = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: "0.7rem",
+    width: "14rem",
+  };
 
   return (
     <Box
@@ -17,38 +26,47 @@ function RatingEmployee() {
         p: 4,
       }}
     >
-      <h2>Rate:</h2>
-      <Typography>behavior:</Typography>
-      <Rating
-        sx={{ paddingY: 1 }}
-        size="medium"
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      <Typography>Discipline:</Typography>
-      <Rating
-        sx={{ paddingY: 1 }}
-        size="medium"
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      <Typography>Quality:</Typography>
-      <Rating
-        aria-label="quality"
-        sx={{ paddingY: 1 }}
-        size="medium"
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
+      <h2>
+        Rate
+        <BarChartIcon fontSize="small" />
+      </h2>
+      <Box sx={style}>
+        <Typography>behavior</Typography>
+        <Rating
+          sx={{ paddingY: 1 }}
+          size="medium"
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Box>
+      <Box sx={style}>
+        <Typography>Discipline</Typography>
+        <Rating
+          sx={{ paddingY: 1 }}
+          size="medium"
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Box>
+      <Box sx={style}>
+        <Typography>Quality</Typography>
+        <Rating
+          aria-label="quality"
+          sx={{ paddingY: 1 }}
+          size="medium"
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </Box>
       <DatePicker label="Date Picker" format="YYYY/MM/DD" sx={{ mt: 1 }} />
       <Button sx={{ width: "6rem", mt: 1 }} color="success" variant="contained">
         Register
