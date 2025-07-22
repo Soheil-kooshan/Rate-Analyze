00ee9ea710b0b228804d91497b84b50d8d6d10af
soheil <soheilkooshan710@gmail.com>
2025-07-22 21:09:54 +0330

    get all employees test added

---
+++ server/src/test/java/com/RateEmployees/Rate/Employees/service/EmployeeServiceTest.java
@@ -0,0 +1,49 @@
+package com.RateEmployees.Rate.Employees.service;
+
+
+import com.RateEmployees.Rate.Employees.entity.Employee;
+import com.RateEmployees.Rate.Employees.repository.EmployeeRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTest {
+
+    @Mock
+    private EmployeeRepo employeeRepo;
+
+    @InjectMocks
+    private EmployeeService employeeService;
+
+    @Test
+    void testGetAllEmployees_ReturnsEmployeesList(){
+
+       Employee emp1 = new Employee();
+       emp1.setId(1);
+       emp1.setFullName("soheil kooshan");
+       emp1.setJob("DBA");
+
+       Employee emp2 = new Employee();
+        emp2.setId(1);
+        emp2.setFullName("ali alipoor");
+        emp2.setJob("SEO");
+
+        List<Employee> empList = List.of(emp1 ,emp2);
+        Mockito.when(employeeRepo.findAll()).thenReturn(empList);
+
+        List<Employee> res = employeeService.getEmployees();
+
+        assertEquals(2 , res.size());
+        assertEquals("soheil kooshan" , res.get(0).getFullName());
+        assertEquals("ali alipoor" , res.get(1).getFullName());
+        assertFalse(res.isEmpty());
+    }
+}
