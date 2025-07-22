c4bc49d27e329fd4b4a5281c1d4b584c3c507af4
soheil <soheilkooshan710@gmail.com>
2025-07-22 22:20:23 +0330

    constructor problem fixed

--- server/src/main/java/com/RateEmployees/Rate/Employees/entity/Employee.java
+++ server/src/main/java/com/RateEmployees/Rate/Employees/entity/Employee.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Entity
 @Table(name = "employee_tbl")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 
     @Id
--- server/src/main/java/com/RateEmployees/Rate/Employees/entity/Rate.java
+++ server/src/main/java/com/RateEmployees/Rate/Employees/entity/Rate.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "Rate")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Rate {
 
     @Id
